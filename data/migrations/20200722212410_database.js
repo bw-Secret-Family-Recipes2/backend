6a258806852309956214ded7951553667d8b0c14
AmandaCallender <58009118+AmandaCallender@users.noreply.github.com>
2020-07-23 22:26:11 -0400

    Register & Login Complete

--- data/migrations/20200722212410_database.js
+++ data/migrations/20200722212410_database.js
@@ -1,8 +1,98 @@
-
-exports.up = function(knex) {
+exports.up = async function(knex, Promise) {
+    await knex.schema.createTable('users', tbl => {
+      tbl.increments();
   
-};
-
-exports.down = function(knex) {
+      tbl
+        .string('username', 255)
+        .notNullable()
+        .unique();
   
-};
+      tbl
+        .string('password', 128)
+        .notNullable();
+      
+      tbl.string('title', 128);
+      
+      tbl.string('tagline', 128);
+  
+      tbl.float('timeLength');
+    });
+  
+    await knex.schema.createTable('recipes', tbl => {
+      tbl.increments();
+  
+      tbl
+        .integer('user_id')
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+  
+      tbl
+        .string('title')
+        .notNullable()
+        .unique();
+      
+      tbl.string('source');
+  
+      tbl.string('notes');
+    });
+  
+    await knex.schema.createTable('ingredients', tbl => {
+      tbl.increments();
+  
+      tbl
+        .integer('recipe_id')
+        .notNullable()
+        .references('id')
+        .inTable('recipes')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+  
+      tbl
+        .string('name')
+        .notNullable();
+    });
+  
+    await knex.schema.createTable('instructions', tbl => {
+      tbl.increments();
+  
+      tbl
+        .integer('recipe_id')
+        .notNullable()
+        .references('id')
+        .inTable('recipes')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+  
+      tbl
+        .string('name')
+        .notNullable();
+    });
+  
+    await knex.schema.createTable('tags', tbl => {
+      tbl.increments();
+  
+      tbl
+        .integer('recipe_id')
+        .notNullable()
+        .references('id')
+        .inTable('recipes')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+  
+      tbl
+        .string('tag')
+        .notNullable();
+    });
+  }
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema
+      .dropTableIfExists('users')
+      .dropTableIfExists('recipes')
+      .dropTableIfExists('ingredients')
+      .dropTableIfExists('instructions')
+      .dropTableIfExists('tags');
+  };
